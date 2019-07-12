5bbcb6d667659a9769d585ac0193dd6d8ffe0fc4
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-07-12 14:58:05 -0300

    +Plano, esfera, cubo
    Referente as Aulas 1 e 2 do canal Brackeys (How to make a Video Game in Unity).

---
+++ primeiro-game/Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour{
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start(){
+        Debug.Log("Iniciando");
+        rb.useGravity = true;
+        rb.AddForce(0, 100, 0);
+    }
+
+    // Update is called once per frame
+    void Update(){ //É chamado a cada frame produzido.
+        rb.AddForce(0, 0, 10 * Time.deltaTime); //Time.deltaTime serve para manter a proporção de acordo com a variação dos frames produzidos(Se baseando no time do último frame).
+
+    }
+}
