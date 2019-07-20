ae82f18d20d7b53bf9bb42772d5be2b246abf8fb
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-07-20 20:54:50 -0300

    #Level2, Level3, LevelComplete, Sound

--- primeiro-game/Assets/Scripts/PlayerMoviment.cs
+++ primeiro-game/Assets/Scripts/PlayerMoviment.cs
@@ -19,8 +19,10 @@ public class PlayerMoviment : MonoBehaviour{
 
     //É chamado a cada frame produzido.
     void Update(){
-        rb.AddForce(0, 0, force * Time.deltaTime); //Time.deltaTime serve para manter a proporção de acordo com a variação dos frames produzidos(Se baseando no time do último frame).
-
+        Debug.Log(rb.velocity.z);
+        if (rb.velocity.z < 45){
+            rb.AddForce(0, 0, force * Time.deltaTime); //Time.deltaTime serve para manter a proporção de acordo com a variação dos frames produzidos(Se baseando no time do último frame).
+        }
     }
 
     private void FixedUpdate(){
