dc69b802cd88111337b1c2c06fa799019a0195f1
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-07-20 18:14:52 -0300

    +Level3, MovableCube

---
+++ primeiro-game/Assets/Scripts/AnimationCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCube : MonoBehaviour{
+
+    public Transform MovableCube;
+    public Rigidbody rb;
+    public float lateralForce;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(MovableCube.position.x <= -4.0 ){
+            rb.AddForce(lateralForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (MovableCube.position.x >= 4.0){
+            rb.AddForce(-lateralForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
